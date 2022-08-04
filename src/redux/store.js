d7094fa71753fd4a1924a233837d0494d58c2bf9
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-08-04 10:59:04 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
 // const import { reducerName } from './reducer'
 import { createStore } from 'redux';
+//-----------------------------------------------------//
+const InitialState = {
+  contacts: {
+    items: [],
+    filter: '',
+  },
+};
 const reducer = (state = {}, { type, payload }) => state;
-const store = createStore(reducer);
+const store = createStore(reducer, InitialState);
 
 export default store;
